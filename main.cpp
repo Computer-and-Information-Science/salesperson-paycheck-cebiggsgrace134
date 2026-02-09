b7b245542ed38eab867a96a88cddbbacfff762a8
cebiggsgrace134 <catherinelizabeth.biggsgrace@student.northampton.edu>
2026-02-09 00:29:45 +0000

    final trial. I don't think I declared the variables properly and I am unsure how to properly format the program.

--- main.cpp
+++ main.cpp
@@ -22,24 +22,20 @@ int main()
   double totalSales;
   double expenses;
 
-  double commission;
-  double total;
-
+  double total, commission;
 
 //reading in employees
   //first employee
   outData << fixed << showpoint;
   outData << setprecision (2);
 
-  cout << "Processing data" << endl;
-
   inData >> firstName >> lastName >> baseSalary >> totalSales >> commissionPercent>> expenses;
   inData >> baseSalary >> commissionPercent;
   inData >> commission;
   inData >> expenses;
 
-  //commission = totalSales*commissionPercent;
-  total = baseSalary + (totalSales*commissionPercent) + expenses; 
+  commission = totalSales*commissionPercent;
+  total = baseSalary+(totalSales*commissionPercent)+expenses; 
 
   outData << "Payroll data for " << setw(13)<<  firstName << " " << lastName << endl;
   outData << " " << endl;
@@ -52,35 +48,48 @@ int main()
 
 
   //second employee
-  inData >> lastName >> firstName >> baseSalary >> totalSales >> commissionPercent>> expenses;
+
+
+  outData << fixed << showpoint;
+  outData << setprecision (2);
+
+  inData >> firstName >> lastName >> baseSalary >> totalSales >> commissionPercent>> expenses;
   inData >> baseSalary >> commissionPercent;
-  inData >> totalSales;
+  inData >> commission;
   inData >> expenses;
 
   commission = totalSales*commissionPercent;
   total = baseSalary + (totalSales*commissionPercent) + expenses; 
 
-  outData << firstName << lastName << endl;
-  outData << baseSalary << commissionPercent << endl;
-  outData << totalSales << endl;
-  outData << expenses << endl;
-  outData << total << endl;
+  outData << "Payroll data for " << setw(13)<<  firstName << " " << lastName << endl;
+  outData << " " << endl;
+  outData << "Base Salary: " << setw(10)<< baseSalary << endl;
+  outData << "Commision: " << setw(12) << commission << " (" << commissionPercent << "%" << " of " << totalSales << ")"<< endl;
+  outData << "Expenses: " << setw(13) << expenses << endl;
+  //outData << "    ------" << setw(13) << endl;
+  outData << "Total: " << setw(16) << total << endl;
 
 
   //third employee
-  inData >> lastName >> firstName >> baseSalary >> totalSales >> commissionPercent>> expenses;
+
+  outData << fixed << showpoint;
+  outData << setprecision (2);
+
+  inData >> firstName >> lastName >> baseSalary >> totalSales >> commissionPercent>> expenses;
   inData >> baseSalary >> commissionPercent;
-  inData >> totalSales;
+  inData >> commission;
   inData >> expenses;
 
   commission = totalSales*commissionPercent;
   total = baseSalary + (totalSales*commissionPercent) + expenses; 
 
-  outData << firstName << lastName << endl;
-  outData << baseSalary << commissionPercent << endl;
-  outData << totalSales << endl;
-  outData << expenses << endl;
-  outData << total << endl;
+  outData << "Payroll data for " << setw(13)<<  firstName << " " << lastName << endl;
+  outData << " " << endl;
+  outData << "Base Salary: " << setw(10)<< baseSalary << endl;
+  outData << "Commision: " << setw(12) << commission << " (" << commissionPercent << "%" << " of " << totalSales << ")"<< endl;
+  outData << "Expenses: " << setw(13) << expenses << endl;
+  //outData << "    ------" << setw(13) << endl;
+  outData << "Total: " << setw(16) << total << endl;
 
 //closing files
 
